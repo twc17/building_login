2c8290bd345b77350b63b5f78ba396b3f67033af
Troy <troy.caro@pitt.edu>
2017-09-19 15:37:47 -0400

    Initial program commit

---
+++ building_access.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+# Title: building_login.py
+# Author: Troy <twc17@pitt.edu>
+# Date Modified: 09/19/2017
+# Version: 1.0.0
+# 
+# Purpose:
+#   This is a program for a building access log book. It uses a magnetic card reader to grab
+#   a Pitt employees 2P number from their ID card. This will then be used for an LDAP query to
+#   get the rest of their account information. You also have the option to manually enter in 
+#   information for guests, or if you forgot your ID card.
+#
+# Usage:
+#   python [-h] building_login.py
+#
+# TODO: Everything! lololol
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
